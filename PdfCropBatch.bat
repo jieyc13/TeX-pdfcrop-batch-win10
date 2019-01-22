41dc27cae8740efff3898c766441e7d2fae06770
jieyc13 <46725289+jieyc13@users.noreply.github.com>
2019-01-22 20:18:10 +0100

    Update PdfCropBatch.bat

--- PdfCropBatch.bat
+++ PdfCropBatch.bat
@@ -1,5 +1,5 @@
 @echo off
-rem 指定存放文件的目录
+rem 指定存放文件的目录/ Give the name of the folder.
 set FolderName=C:\Yucheng\TeX_Train\FigArray\fig\tmpPdfCrop
 for /f "delims=\" %%a in ('dir /b /a-d /o-d "%FolderName%\*.pdf"') do (
   pdfcrop %%a %%a
